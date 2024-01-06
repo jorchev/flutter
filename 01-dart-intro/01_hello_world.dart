e32e9ee865134831b7d08826ffcd1a98ca7d681e
Jorge Vega <j.vega@mafasa.org>
2024-01-06 08:30:37 -0700

    Hola Mundo - Dart

---
+++ 01-dart-intro/01_hello_world.dart
@@ -0,0 +1,15 @@
+void main() {
+  
+  //var myName = 'Jorge';
+  //String myName = 'Jorge';
+  //final myName = 'Jorge';
+  //late final myName;
+  //late String myName;
+  const myName = 'Fernando';
+  
+  //myName = 'Mundo';
+  
+  print('Hola $myName');
+  print('Hola ${ myName.toUpperCase() }');
+  print('Hola ${ 1 + 1 }');
+}
