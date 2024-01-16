48d3f52425c8ec6b641a6d72d51c32828b5db3c1
Jorge Vega <j.vega@mafasa.org>
2024-01-16 14:17:54 -0700

    Clases abstractas y enumaraciones

---
+++ 01-dart-intro/10_abstract_class.dart
@@ -0,0 +1,21 @@
+void main() {
+
+  // final windPlant = EnergyPlant(); // esto no funciona
+
+}
+
+enum PlantType { nuclear, wind, water }
+
+abstract class EnergyPlant {
+
+  double energyLeft;
+  PlantType type; // nuclear, wind, water
+
+  EnergyPlant({
+    required this.energyLeft,
+    required this.type
+  });
+
+  void consumeEnergy( double amount );
+
+}
