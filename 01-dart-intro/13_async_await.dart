c8cea8135c037107d0ff323ba83ce15a9eae9808
Jorge Vega <jorge.hvr@gmail.com>
2024-01-18 19:09:32 -0700

    Async - Await

---
+++ 01-dart-intro/13_async_await.dart
@@ -0,0 +1,20 @@
+void main() async {
+  print('Inicio del programa');
+
+  try{
+    final value = await httpGet('http://evo-code.com/cursos');
+    print( value );
+  }catch(err){
+    print('Tenemos un error: $err');
+  }
+
+
+  print('Fin del programa');
+}
+
+Future<String> httpGet( String url ) async {
+  await Future.delayed( Duration(seconds: 1));
+
+  throw 'Error en la petición';
+  return 'Tenemos un valor de la petición';
+}
