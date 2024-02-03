f8ca46e3da62ffe8715f8e3a2be600ff20e6e3d4
Jorge Vega <j.vega@mafasa.org>
2024-02-03 12:04:27 -0700

    Material Design 3

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -15,9 +15,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CounterScreen()
+      theme: ThemeData(
+        colorSchemeSeed: Colors.blue
+      ),
+      home: const CounterScreen()
     );
   }
 
