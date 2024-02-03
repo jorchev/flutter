adf2b3799f59665fbd0d46f41a6e5dee08aa4480
Jorge Vega <j.vega@mafasa.org>
2024-02-03 11:05:22 -0700

    Estructura de directorios - Nueva Pantalla

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world_app/presentation/screens/counter/counter_screen.dart';
 
 void main(){
   
@@ -16,9 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(child: Text('Hola Mundo')),
-      )
+      home: CounterScreen()
     );
   }
 
