586c8f890539ef6fa5039e5967bea1db149cc1a8
Jorge Vega <j.vega@mafasa.org>
2024-02-03 12:02:41 -0700

    Contador - Diseño de pantalla

--- hello_world_app/lib/presentation/screens/counter/counter_screen.dart
+++ hello_world_app/lib/presentation/screens/counter/counter_screen.dart
@@ -5,8 +5,25 @@ class CounterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(child: Text('Counter Screen')),
+    return Scaffold(
+        appBar: AppBar(
+          title: const Center(child: Text('Counter Screen')),
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('10', style: TextStyle( fontSize: 160, fontWeight: FontWeight.w100 )),
+              Text('Clicks', style: TextStyle( fontSize: 25 ))
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+
+          },
+          child: const Icon( Icons.plus_one ),
+        ),
       );
   }
 }
