93281df829492c5ae5408daeed608011a8451ca5
Jorge Vega <j.vega@mafasa.org>
2024-02-10 08:31:27 -0700

    Mis mensajes - Burbuja de chat

--- yes_no_app/lib/presentation/screens/chat/chat_screen.dart
+++ yes_no_app/lib/presentation/screens/chat/chat_screen.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -36,7 +37,7 @@ class _ChatView extends StatelessWidget {
             Expanded(child: ListView.builder(
               itemCount: 100,
               itemBuilder: (context, index) {
-              return Text('Indice: $index');
+              return const MyMessageBubble();
             })),
 
             const Text('Mundo')
