ba67784f87951ab56e1233a4294e4f0ace5c48d8
Jorge Vega <j.vega@mafasa.org>
2024-02-22 13:02:14 -0700

    Mappers

--- yes_no_app/lib/config/helpers/get_yes_no_answer.dart
+++ yes_no_app/lib/config/helpers/get_yes_no_answer.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/infrastructure/models/yes_no_model.dart';
 
 class GetYesNoAnswer {
   
@@ -8,7 +9,14 @@ class GetYesNoAnswer {
   Future<Message> getAnswer() async {
     final response = await _dio.get('https://yesno.wtf/api');
 
-    throw UnimplementedError();
+    final yesNoModel = YesNoModel.fromJsonMap(response.data);
+
+
+    return Message(
+      text: yesNoModel.answer, 
+      fromWho: FromWho.hers,
+      imageUrl: yesNoModel.image
+    );
   }
 
 }
