7a117e3284152b354f8458a9bc9d1c85fc7edb72
Jorge Vega <j.vega@mafasa.org>
2024-02-22 13:21:08 -0700

    QuickType.io al rescate

--- yes_no_app/lib/config/helpers/get_yes_no_answer.dart
+++ yes_no_app/lib/config/helpers/get_yes_no_answer.dart
@@ -12,11 +12,7 @@ class GetYesNoAnswer {
     final yesNoModel = YesNoModel.fromJsonMap(response.data);
 
 
-    return Message(
-      text: yesNoModel.answer, 
-      fromWho: FromWho.hers,
-      imageUrl: yesNoModel.image
-    );
+    return yesNoModel.toMessageEntity();
   }
 
 }
