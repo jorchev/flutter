9ab645dfebb7a8e468491c15e5f0bb809190e12d
Jorge Vega <jorge.hvr@gmail.com>
2024-05-06 17:27:18 -0700

    Botones de likes y views

---
+++ toktik/lib/presentation/widgets/shared/video_buttons.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+
+class VideoButtons extends StatelessWidget {
+  VideoPost video;
+
+  VideoButtons({super.key, required this.video});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _CustomIconButton(value: video.likes, iconData: Icons.favorite, iconColor: Colors.red),
+        _CustomIconButton(value: video.views, iconData: Icons.remove_red_eye_outlined )
+      ],
+    );
+  }
+}
+
+class _CustomIconButton extends StatelessWidget {
+  int value;
+  IconData iconData;
+  Color? color;
+
+  _CustomIconButton(
+    {
+      super.key,
+      required this.value,
+      required this.iconData,
+      iconColor
+    }): color = iconColor ?? Colors.white;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        IconButton(
+            onPressed: () {},
+            icon: Icon(
+              iconData,
+              color: color,
+              size: 30,
+            )),
+            Text('$value')
+      ],
+    );
+  }
+}
--- toktik/lib/presentation/widgets/shared/video_scrollable_view.dart
+++ toktik/lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
 
 class VideoScrollableView extends StatelessWidget {
-  const VideoScrollableView({
-    super.key, 
-    required this.videos
-  });
+  const VideoScrollableView({super.key, required this.videos});
 
   final List<VideoPost> videos;
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
+    return PageView.builder(
       physics: const BouncingScrollPhysics(),
       scrollDirection: Axis.vertical,
-      children: [
-        Container( color: Colors.red ),
-        Container( color: Colors.blue),
-        Container( color: Colors.teal),
-        Container( color: Colors.yellow),
-        Container( color: Colors.pink),
-        Container( color: Colors.deepPurple),
-      ],
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+        final VideoPost videoPost = videos[index];
+        return Stack(
+          children: [
+            Positioned(
+                bottom: 40, right: 20, child: VideoButtons(video: videoPost)),
+          ],
+        );
+      },
     );
   }
 }
